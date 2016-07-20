08b68b03a5bd07d0a62eb55d4856b4586915c891
NikitaJTelisara <nikitatelisara@gmail.com>
2016-07-19 22:58:52 -0500

    Add files via upload

---
+++ createStairCase.java
@@ -0,0 +1,38 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: NT030798
+ * Date: 7/19/16
+ * Time: 9:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class createStairCase {
+
+    public static void main(String[] args) {
+        createStairCase(6);
+    }
+
+    public static void createStairCase(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+/* output
+
+#
+##
+###
+####
+#####
+######
+
+
+ */
